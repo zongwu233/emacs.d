80037aeff8351f78bd9dcefe9e251ce304cb36ad
zongwu233 <marquisx2x@163.com>
2023-09-28 20:33:48 +0800

    add new

--- init.el
+++ init.el
@@ -120,16 +120,36 @@ Other buffer group by `awesome-tab-get-group-name' with project name."
      "OrgMode")
     (t
      (awesome-tab-get-group-name (current-buffer))))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(consult embark vertico orderless marginalia embark-consult company)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;;禁止自动生成备份文件
+(setq make-backup-files nil)
+
+;;最近打开文件
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-item 10)
+(global-set-key (kbd "C-x b") 'consult-buffer)
+
+(delete-selection-mode 1)
+
+(defun consult-directory-externally (file)
+  "Open FILE externally using the default application of the system."
+  (interactive "fOpen externally: ")
+  (if (and (eq system-type 'windows-nt)
+	   (fboundp 'w32-shell-execute))
+      (shell-command-to-string (encode-coding-string (replace-regexp-in-string "/" "\\\\"
+									       (format "explorer.exe %s" (file-name-directory (expand-file-name file)))) 'gbk))
+    (call-process (pcase system-type
+		    ('darwin "open")
+		    ('cygwin "cygstart")
+		    (_ "xdg-open"))
+		  nil 0 nil
+		  (file-name-directory (expand-file-name file)))))
+;; 给 embark 添加一个 action: E 使用系统的文件浏览器打开当前目录
+;; https://github.com/oantolin/embark/tree/fcf068c0e912baa9267cbec02e56a5972d8f31b4#creating-your-own-keymaps
+(eval-after-load 'embark
+  (defvar-keymap embark-file-map "E" #'consult-directory-externally))
+;;打开当前文件的目录
+(defun my-open-current-directory ()
+  (interactive)
+  (consult-directory-externally default-directory))
