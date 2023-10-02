81842f7c09a3d182bc664ce81c88bb7e7be85ddd
zongwu233 <marquisx2x@163.com>
2023-10-02 22:34:30 +0800

    reinstall org  and org contrib latest version

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -1,12 +1,20 @@
 (require 'package)
 (setq package-archives '(("gnu"   . "https://mirrors.163.com/elpa/gnu/")
-
 			 ("melpa" . "https://mirrors.163.com/elpa/melpa/")))
+;; add nongnu resource
+(add-to-list 'package-archives
+	    (cons "nongnu" (format "http%s://elpa.nongnu.org/nongnu/"
+				   (if (gnutls-available-p) "s" ""))))
+
 (package-initialize)
 
 ;;防止反复调用 package-refresh-contents 会影响加载速度
 (when (not package-archive-contents)
   (package-refresh-contents))
+
+;;; 这个配置一定要配置在 use-package 的初始化之前，否则无法正常安装
+(assq-delete-all 'org package--builtins)
+(assq-delete-all 'org package--builtin-versions)
 
 
 ;; Setup `use-package'
