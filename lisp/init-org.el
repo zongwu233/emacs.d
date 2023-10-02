31b89395356ddf0d6e0ffe07afc1ca58cc995c6a
zongwu233 <marquisx2x@163.com>
2023-10-02 16:36:05 +0800

    fix electric-pair-inhibit-predicate variable void

---
+++ lisp/init-org.el
@@ -0,0 +1,14 @@
+(require 'org-tempo)  ;开启easy template
+
+;; 禁用左尖括号
+(setq electric-pair-inhibit-predicate
+      `(lambda (c)
+	 (if (char-equal c ?\<) t (,electric-pair-inhibit-predicate c))))
+
+(add-hook 'org-mode-hook
+	  (lambda ()
+	    (setq-local electric-pair-inhibit-predicate
+			`(lambda (c)
+			   (if (char-equal c ?\<) t (,electric-pair-inhibit-predicate c))))))
+
+(provide 'init-org)
