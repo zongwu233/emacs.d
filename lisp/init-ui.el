4c6bd8dbadb97d93f45b36d334125ca915801868
zongwu233 <marquisx2x@163.com>
2023-10-04 00:14:15 +0800

    fix for doom modeline

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -4,7 +4,7 @@
 (load-theme 'gruvbox-light-soft t)
 (use-package doom-modeline
   :ensure t
-  ;fix doom modeline 
+					;fix doom modeline 
   :custom-face
   (mode-line ((t (:height 0.9))))
   (mode-line-inactive ((t (:height 0.9))))
@@ -16,8 +16,16 @@
   :init
   (progn
     (setq column-number-mode t)
-  ))
+    ))
 
+(use-package nerd-icons
+  :ensure t
+  ;; :custom
+  ;; The Nerd Font you want to use in GUI
+  ;; "Symbols Nerd Font Mono" is the default and is recommended
+  ;; but you can use any other Nerd Font if you want
+  ;; (nerd-icons-font-family "Symbols Nerd Font Mono")
+  )
 
 
 
