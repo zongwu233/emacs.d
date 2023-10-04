d3ed7ccab8a3c0a65d5b436e5c04fa3485f09dc4
zongwu233 <marquisx2x@163.com>
2023-10-04 15:48:23 +0800

    add iedit

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -48,41 +48,41 @@
 
 ;;文本搜索  doom modeline 会显示结果总数以及当前 index
 (use-package evil-anzu
-:ensure t
-:after evil
-:diminish
-:demand t
-:init
-(global-anzu-mode t))
+  :ensure t
+  :after evil
+  :diminish
+  :demand t
+  :init
+  (global-anzu-mode t))
 
 ;;社区更统一的定义各种常用 mode 下的 evil state
 (use-package evil-collection
-:ensure t
-:demand t
-:config
-(setq evil-collection-mode-list (remove 'lispy evil-collection-mode-list))
-(evil-collection-init)
+  :ensure t
+  :demand t
+  :config
+  (setq evil-collection-mode-list (remove 'lispy evil-collection-mode-list))
+  (evil-collection-init)
 
-(cl-loop for (mode . state) in
-	 '((org-agenda-mode . normal)
-	   (Custom-mode . emacs)
-	   (eshell-mode . emacs)
-	   (org-mode . emacs)
-	   (makey-key-mode . motion))
-	 do (evil-set-initial-state mode state)))
+  (cl-loop for (mode . state) in
+	   '((org-agenda-mode . normal)
+	     (Custom-mode . emacs)
+	     (eshell-mode . emacs)
+	     (org-mode . emacs)
+	     (makey-key-mode . motion))
+	   do (evil-set-initial-state mode state)))
 
 ;; vim surround plugin
 (use-package evil-surround
- :ensure t
- :init
- (global-evil-surround-mode 1))
+  :ensure t
+  :init
+  (global-evil-surround-mode 1))
 
 (use-package evil-nerd-commenter
   :ensure t
   :init
   (define-key evil-normal-state-map (kbd ",/") 'evilnc-comment-or-uncomment-lines)
   (define-key evil-visual-state-map (kbd ",/") 'evilnc-comment-or-uncomment-lines)
-)
+  )
 (use-package evil-snipe
   :ensure t
   :diminish
@@ -95,6 +95,24 @@
   :ensure
   :init
   (global-evil-matchit-mode 1))
+
+;; emacs 也有 multicusor 但是类 vim 的这个 iedit 操作更符合 vim 习惯
+;; 在 normal 模式按 vim 选择当前 word，按 R 则会选择文本中所有的该 word
+;; 此时可以按 gg 或者 G 跳转到匹配的 word 第一个 或者最后一个
+;; 或者 0 $ 跳转到 word 的第一个单词/最后一个单词
+(use-package iedit
+  :ensure t
+  :init
+  (setq iedit-toggle-key-default nil)
+  :config
+  (define-key iedit-mode-keymap (kbd "M-h") 'iedit-restrict-function)
+  (define-key iedit-mode-keymap (kbd "M-i") 'iedit-restrict-current-line))
+
+(use-package evil-multiedit
+  :ensure t
+  :commands (evil-multiedit-default-keybinds)
+  :init
+  (evil-multiedit-default-keybinds))
 
 
 
