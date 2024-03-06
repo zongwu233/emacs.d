3d4dc57ade329333e779794059b1de64fdfb2dbd
zongwu233 <marquisx2x@163.com>
2024-03-06 20:54:18 +0800

    fix Fcitx input method cannot use .

--- lisp/init-search.el
+++ lisp/init-search.el
@@ -1,37 +1,37 @@
- ;; -*- coding: utf-8; lexical-binding: t; -*-
+;; -*- coding: utf-8; lexical-binding: t; -*-
 
-  (use-package embark-consult :ensure t)
-  (use-package wgrep :ensure t)
-  (setq wgrep-auto-save-buffer t)
+ (use-package embark-consult :ensure t)
+ (use-package wgrep :ensure t)
+ (setq wgrep-auto-save-buffer t)
 
-  (eval-after-load
-      'consult
-    '(eval-after-load
-	 'embark
-       '(progn
-	  (require 'embark-consult)
-	  (add-hook
-	   'embark-collect-mode-hook
-	   #'consult-preview-at-point-mode))))
+ (eval-after-load
+     'consult
+   '(eval-after-load
+	'embark
+      '(progn
+	 (require 'embark-consult)
+	 (add-hook
+	  'embark-collect-mode-hook
+	  #'consult-preview-at-point-mode))))
 
-  (defun embark-export-write ()
-  "Export the current vertico results to a writable buffer if possible.
-  Supports exporting consult-grep to wgrep, file to wdeired, and consult-location to occur-edit"
-  (interactive)
-  (require 'embark)
-  (require 'wgrep)
-  (pcase-let ((`(,type . ,candidates)
-	       (run-hook-with-args-until-success 'embark-candidate-collectors)))
-    (pcase type
-      ('consult-grep (let ((embark-after-export-hook #'wgrep-change-to-wgrep-mode))
-		       (embark-export)))
-      ('file (let ((embark-after-export-hook #'wdired-change-to-wdired-mode))
-	       (embark-export)))
-      ('consult-location (let ((embark-after-export-hook #'occur-edit-mode))
-			   (embark-export)))
-      (x (user-error "embark category %S doesn't support writable export" x)))))
+ (defun embark-export-write ()
+ "Export the current vertico results to a writable buffer if possible.
+ Supports exporting consult-grep to wgrep, file to wdeired, and consult-location to occur-edit"
+ (interactive)
+ (require 'embark)
+ (require 'wgrep)
+ (pcase-let ((`(,type . ,candidates)
+	      (run-hook-with-args-until-success 'embark-candidate-collectors)))
+   (pcase type
+     ('consult-grep (let ((embark-after-export-hook #'wgrep-change-to-wgrep-mode))
+		      (embark-export)))
+     ('file (let ((embark-after-export-hook #'wdired-change-to-wdired-mode))
+	      (embark-export)))
+     ('consult-location (let ((embark-after-export-hook #'occur-edit-mode))
+			  (embark-export)))
+     (x (user-error "embark category %S doesn't support writable export" x)))))
 
-  (define-key minibuffer-local-map (kbd "C-c C-e") 'embark-export-write)
+ (define-key minibuffer-local-map (kbd "C-c C-e") 'embark-export-write)
 
 
-  (provide 'init-search)
+ (provide 'init-search)
