1bedc5e3432250210a1c6c772c3e4fee5b625fd0
zongwu233 <marquisx2x@163.com>
2025-11-23 21:49:31 +0800

    maocs newline

--- lisp/init-begin.el
+++ lisp/init-begin.el
@@ -1,11 +1,11 @@
-;; -*- coding: utf-8; lexical-binding: t; -*-
+  ;; -*- coding: utf-8; lexical-binding: t; -*-
 
-;; hidden menu-bar tool-bar scroll-bar, not nil ,not negative, specially apply in client mode.
+  ;; hidden menu-bar tool-bar scroll-bar, not nil ,not negative, specially apply in client mode.
 
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
+  (push '(menu-bar-lines . 0) default-frame-alist)
+  (push '(tool-bar-lines . 0) default-frame-alist)
+  (push '(vertical-scroll-bars) default-frame-alist)
 
-;; full screen emacs
-(toggle-frame-fullscreen)
-(provide 'init-begin)
+  ;; full screen emacs
+  (toggle-frame-fullscreen)
+  (provide 'init-begin)
