42491e49be0c0f734f424c8caadf711ab3ede414
zongwu233 <marquisx2x@163.com>
2025-11-26 17:39:05 +0800

    fix ^M char

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -11,76 +11,76 @@
     (setq company-minimum-prefix-length 1)
     (setq company-idle-delay 0))
 
-  ;;增强 minibuffer 补全
-  (use-package vertico :ensure	t)
-  (vertico-mode t)
+;;增强 minibuffer 补全
+(use-package vertico :ensure	t)
+(vertico-mode t)
 
-  ;; make c-j/c-k work in vertico selection
-  (define-key vertico-map (kbd "C-j") 'vertico-next)
-  (define-key vertico-map (kbd "C-k") 'vertico-previous)
+;; make c-j/c-k work in vertico selection
+(define-key vertico-map (kbd "C-j") 'vertico-next)
+(define-key vertico-map (kbd "C-k") 'vertico-previous)
 
-  ;;在 minibuffer 支持模糊搜索
-  (use-package orderless :ensure t)
-  (setq completion-styles '(orderless))
+;;在 minibuffer 支持模糊搜索
+(use-package orderless :ensure t)
+(setq completion-styles '(orderless))
 
-  ;; 增强 minibuffer 的annotation
-  (use-package marginalia :ensure t)
-  (marginalia-mode t)
+;; 增强 minibuffer 的annotation
+(use-package marginalia :ensure t)
+(marginalia-mode t)
 
-  ;; minibuffer action 就类似于在任何内容上右键点击
-  (use-package embark :ensure t)
-  (global-set-key (kbd "C-;") 'embark-act)
-  ;;利用这个 可以不用再记忆快捷键，只需要执行 C-x C-h 呼出帮助命令
-  ;;然后输入想执行命令的关键字，既可以出现提示列表，又因为 orderless
-  ;; 可以不用在意关键字单词的顺序 最后直接回车执行即可
-  (setq prefix-help-command 'embark-prefix-help-command)
+;; minibuffer action 就类似于在任何内容上右键点击
+(use-package embark :ensure t)
+(global-set-key (kbd "C-;") 'embark-act)
+;;利用这个 可以不用再记忆快捷键，只需要执行 C-x C-h 呼出帮助命令
+;;然后输入想执行命令的关键字，既可以出现提示列表，又因为 orderless
+;; 可以不用在意关键字单词的顺序 最后直接回车执行即可
+(setq prefix-help-command 'embark-prefix-help-command)
 
-  ;;增强文件内搜索和跳转函数定义
-  (use-package consult :ensure t)
-  ;;同时使用需要这个
-  (use-package embark-consult :ensure t)
-  ;;强大的文件内搜索功能，完全替代了 swiper 插件
-  (global-set-key (kbd "C-s") 'consult-line)
-  ;;文件内函数跳转
-  ;;consult-imenu
+;;增强文件内搜索和跳转函数定义
+(use-package consult :ensure t)
+;;同时使用需要这个
+(use-package embark-consult :ensure t)
+;;强大的文件内搜索功能，完全替代了 swiper 插件
+(global-set-key (kbd "C-s") 'consult-line)
+;;文件内函数跳转
+;;consult-imenu
 
-  ;; consult 默认配置优化
-  (eval-after-load 'consult
-  (progn
-    (setq
-     consult-narrow-key "<"
-     consult-line-numbers-widen t
-     consult-async-min-input 2
-     consult-async-refresh-delay  0.15
-     consult-async-input-throttle 0.2
-     consult-async-input-debounce 0.1)
-    ))
+;; consult 默认配置优化
+(eval-after-load 'consult
+(progn
+  (setq
+   consult-narrow-key "<"
+   consult-line-numbers-widen t
+   consult-async-min-input 2
+   consult-async-refresh-delay  0.15
+   consult-async-input-throttle 0.2
+   consult-async-input-debounce 0.1)
+  ))
 
-  (defun consult-directory-externally (file)
-    "Open FILE externally using the default application of the system."
-    (interactive "fOpen externally: ")
-    (if (and (eq system-type 'windows-nt)
-	     (fboundp 'w32-shell-execute))
-	(shell-command-to-string (encode-coding-string
-				  (replace-regexp-in-string "/" "\\\\"
-				    (format "explorer.exe %s"
-					    (file-name-directory
-					  (expand-file-name file)))) 'gbk))
-      (call-process (pcase system-type
-		      ('darwin "open")
-		      ('cygwin "cygstart")
-		      (_ "xdg-open"))
-		    nil 0 nil
-		    (file-name-directory (expand-file-name file)))))
-  ;; 给 embark 添加一个 action: E 使用系统的文件浏览器打开当前目录
-  ;; https://github.com/oantolin/embark/tree/fcf068c0e912baa9267cbec02e56a5972d8f31b4#creating-your-own-keymaps
-  (eval-after-load 'embark
-    (defvar-keymap embark-file-map "E" #'consult-directory-externally))
-  ;;打开当前文件的目录
-  (defun my-open-current-directory ()
-    (interactive)
-    (consult-directory-externally default-directory))
+(defun consult-directory-externally (file)
+  "Open FILE externally using the default application of the system."
+  (interactive "fOpen externally: ")
+  (if (and (eq system-type 'windows-nt)
+	   (fboundp 'w32-shell-execute))
+  (shell-command-to-string (encode-coding-string
+				(replace-regexp-in-string "/" "\\\\"
+				  (format "explorer.exe %s"
+					  (file-name-directory
+					(expand-file-name file)))) 'gbk))
+    (call-process (pcase system-type
+		    ('darwin "open")
+		    ('cygwin "cygstart")
+		    (_ "xdg-open"))
+		  nil 0 nil
+		  (file-name-directory (expand-file-name file)))))
+;; 给 embark 添加一个 action: E 使用系统的文件浏览器打开当前目录
+;; https://github.com/oantolin/embark/tree/fcf068c0e912baa9267cbec02e56a5972d8f31b4#creating-your-own-keymaps
+(eval-after-load 'embark
+  (defvar-keymap embark-file-map "E" #'consult-directory-externally))
+;;打开当前文件的目录
+(defun my-open-current-directory ()
+  (interactive)
+  (consult-directory-externally default-directory))
 
 
 
-  (provide 'init-completion)
+(provide 'init-completion)
--- lisp/init-tabs.el
+++ lisp/init-tabs.el
@@ -1,54 +1,54 @@
-  ;; Windows layout recorder
-  ;;
-  ;; You can still use `winner-mode' on Emacs 26 or early. On Emacs 27, it's
-  ;; preferred over `winner-mode' for better compatibility with `tab-bar-mode'.
-  (use-package tab-bar
-    :ensure nil
-    :init  (tab-bar-mode t)
-    ;; 图标太丑了
-    ;;:hook (after-init . tab-bar-history-mode)
-    :custom
-    (tabbar-use-images nil)
-    (tab-bar-new-button-show nil)
-    (tab-bar-close-button-show nil)
-    (tab-bar-new-tab-choice "*scratch*")
-    (tab-bar-tab-hints t)
-   )
+;; Windows layout recorder
+;;
+;; You can still use `winner-mode' on Emacs 26 or early. On Emacs 27, it's
+;; preferred over `winner-mode' for better compatibility with `tab-bar-mode'.
+(use-package tab-bar
+  :ensure nil
+  :init  (tab-bar-mode t)
+  ;; 图标太丑了
+  ;;:hook (after-init . tab-bar-history-mode)
+  :custom
+  (tabbar-use-images nil)
+  (tab-bar-new-button-show nil)
+  (tab-bar-close-button-show nil)
+  (tab-bar-new-tab-choice "*scratch*")
+  (tab-bar-tab-hints t)
+ )
 
-  (use-package tabspaces
-    :ensure t
-    :hook (after-init . tabspaces-mode) ;; use this only if you want the minor-mode loaded at startup.
-    :commands (tabspaces-switch-or-create-workspace
-	       tabspaces-open-or-create-project-and-workspace)
-    :custom
-    (tabspaces-use-filtered-buffers-as-default t)
-    (tabspaces-default-tab "Default")
-    (tabspaces-remove-to-default t)
-    (tabspaces-include-buffers '("*scratch*"))
-    ;; maybe slow 
-    ;;(tabspaces-session t)
-    ;;(tabspaces-session-auto-restore t)
-    :config
-    ;; Filter Buffers for Consult-Buffer
+(use-package tabspaces
+  :ensure t
+  :hook (after-init . tabspaces-mode) ;; use this only if you want the minor-mode loaded at startup.
+  :commands (tabspaces-switch-or-create-workspace
+	     tabspaces-open-or-create-project-and-workspace)
+  :custom
+  (tabspaces-use-filtered-buffers-as-default t)
+  (tabspaces-default-tab "Default")
+  (tabspaces-remove-to-default t)
+  (tabspaces-include-buffers '("*scratch*"))
+  ;; maybe slow 
+  ;;(tabspaces-session t)
+  ;;(tabspaces-session-auto-restore t)
+  :config
+  ;; Filter Buffers for Consult-Buffer
 
-    (with-eval-after-load 'consult
-      ;; hide full buffer list (still available with "b" prefix)
-      (consult-customize consult--source-buffer :hidden nil :default nil)
-      ;; set consult-workspace buffer list
-      (defvar consult--source-workspace
-	(list :name "Workspace Buffers"
-	      :narrow ?w
-	      :history 'buffer-name-history
-	      :category 'buffer
-	      :state #'consult--buffer-state
-	      :default t
-	      :items (lambda () (consult--buffer-query
-				 :predicate #'tabspaces--local-buffer-p
-				 :sort 'visibility
-				 :as #'buffer-name)))
+  (with-eval-after-load 'consult
+    ;; hide full buffer list (still available with "b" prefix)
+    (consult-customize consult--source-buffer :hidden nil :default nil)
+    ;; set consult-workspace buffer list
+    (defvar consult--source-workspace
+  (list :name "Workspace Buffers"
+	    :narrow ?w
+	    :history 'buffer-name-history
+	    :category 'buffer
+	    :state #'consult--buffer-state
+	    :default t
+	    :items (lambda () (consult--buffer-query
+			   :predicate #'tabspaces--local-buffer-p
+			   :sort 'visibility
+			   :as #'buffer-name)))
 
-	"Set workspace buffer list for consult-buffer.")
-      (add-to-list 'consult-buffer-sources 'consult--source-workspace)))
+  "Set workspace buffer list for consult-buffer.")
+    (add-to-list 'consult-buffer-sources 'consult--source-workspace)))
 
 
-    (provide 'init-tabs)
+  (provide 'init-tabs)
